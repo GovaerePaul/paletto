a16aa8cab3849dc0688e984107824ed20af10917
Paul Govaere <govaere.paul@gmail.com>
2015-11-12 11:44:18 +0100

    Story4-No ok

--- test/test.js
+++ test/test.js
@@ -19,3 +19,15 @@ PalettoTestCase.prototype.testStory3 = function () {
     assertEquals(e.getPlayerPieces(), 1);
     assertEquals(e.addPiecesPlayer(1,"Yellow"), 1);
 };
+
+PalettoTestCase.prototype.testStory4 = function () {
+    assertEquals(e.deletePieces(0,0), 34);
+    e.setPlayerPieces();
+    assertEquals(e.getPlayerPieces(), 1);
+    assertEquals(e.addPiecesPlayer(1,"Black"), 1);
+
+    assertEquals(e.deletePieces(5,5), 33);
+    e.setPlayerPieces();
+    assertEquals(e.getPlayerPieces(), 2);
+    assertEquals(e.addPiecesPlayer(1,"Black"), 2);
+};
