bf9c35808ef09ca82783b3d7331d6ec38cb26e98
Paul Govaere <govaere.paul@gmail.com>
2015-11-12 10:40:52 +0100

    Story3 - No ok

--- test/test.js
+++ test/test.js
@@ -12,3 +12,11 @@ PalettoTestCase.prototype.testStory1 = function () {
 PalettoTestCase.prototype.testStory2 = function () {
     assertEquals(e.isInCorner("Yellow"), true);
 };
+
+PalettoTestCase.prototype.testStory3 = function () {
+    assertEquals(e.getPieces(), 35);
+    assertEquals(e.deletePieces(5,0), 1);
+    assertEquals(e.getPieces(), 35);
+    assertEquals(e.getPlayerPieces(), 1);
+
+};
