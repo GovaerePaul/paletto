166d34ecdb65ba8c6e5025c3489255e1ad58d16e
Paul Govaere <govaere.paul@gmail.com>
2015-11-12 11:44:18 +0100

    Story4-No ok

--- test/test.js
+++ test/test.js
@@ -15,7 +15,21 @@ PalettoTestCase.prototype.testStory2 = function () {
 
 PalettoTestCase.prototype.testStory3 = function () {
     assertEquals(e.deletePieces(5,0), 35);
-    e.setPlayerPieces();
-    assertEquals(e.getPlayerPieces(), 1);
+    e.setPlayer1Pieces();
+    assertEquals(e.getPlayer1Pieces(), 1);
     assertEquals(e.addPiecesPlayer(1,"Yellow"), 1);
+};
+
+PalettoTestCase.prototype.testStory4 = function () {
+    assertEquals(e.checkPossiblePieces(), true);
+
+    assertEquals(e.deletePieces(0,0), 34);
+    e.setPlayer2Pieces();
+    assertEquals(e.getPlayer2Pieces(), 1);
+    assertEquals(e.addPiecesPlayer(1,"Black"), 1);
+
+    assertEquals(e.deletePieces(5,5), 33);
+    e.setPlayer2Pieces();
+    assertEquals(e.getPlayer2Pieces(), 2);
+    assertEquals(e.addPiecesPlayer(1,"Black"), 2);
 };
